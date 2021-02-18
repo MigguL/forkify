95285ef9c6a7c2d06dae2b4850da0e6cddf54b3a
Michal Latos <mlrapid999@gmail.com>
2021-02-18 13:38:23 +0100

    Adding own recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-
 export const TIMEOUT_SEC = 10;
-
 export const RES_PER_PAGE = 15;
+export const KEY = '252becff-fc4e-475c-a7de-cdb4deaa9b32';
+export const MODAL_CLOSE_SEC = 2.5;
--- src/js/views/View.js
+++ src/js/views/View.js
@@ -2,7 +2,6 @@ import icons from 'url:../../img/icons.svg';
 
 export default class View {
   _data;
-
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
